ada4b91b43528439d27423b86605b7a613657e7f
Niko <54240096+Nik0-0@users.noreply.github.com>
2024-07-15 09:44:34 +0200

    Adjust scrolling effect

--- public/js/team_scroll.js
+++ public/js/team_scroll.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     function handleParallaxScroll() {
         parallaxImages.forEach(parallaxImage => {
             const scrollY = window.scrollY;
-            parallaxImage.style.transition = 'transform 0.2s ease-out'; // Adjust the transition timing and easing as needed
-            parallaxImage.style.transform = `translateY(-${scrollY * 0.1}px)`;
+            parallaxImage.style.transition = 'transform 0.8s ease-out';
+            parallaxImage.style.transform = `translateY(-${scrollY * 0.05}px)`;
         });
         requestAnimationFrame(handleParallaxScroll);
     }
