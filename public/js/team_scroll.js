4e84b968683ecc73f9df0088ada5dbcbf7f27a55
Niko <54240096+Nik0-0@users.noreply.github.com>
2024-07-15 12:06:35 +0200

    Set max pos

--- public/js/team_scroll.js
+++ public/js/team_scroll.js
@@ -2,11 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const parallaxImages = document.querySelectorAll('[id^="parallaxImage"]');
 
     function handleParallaxScroll() {
+        const scrollY = window.scrollY;
+        const limitedScrollY = Math.min(Math.max(scrollY * 0.05, -20), 20);
+
         parallaxImages.forEach(parallaxImage => {
-            const scrollY = window.scrollY;
             parallaxImage.style.transition = 'transform 0.8s ease-out';
-            parallaxImage.style.transform = `translateY(-${scrollY * 0.05}px)`;
+            parallaxImage.style.transform = `translateY(-${limitedScrollY}px)`;
         });
+        
         requestAnimationFrame(handleParallaxScroll);
     }
 
