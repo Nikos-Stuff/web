ad86652fa383202a81824825798c8a40d3d6f704
_Nik0__ <maciek.f137@gmail.com>
2024-07-30 09:33:23 +0200

    ✨ Optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import tailwind from "@astrojs/tailwind"
-import solidJs from "@astrojs/solid-js"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikostuff.com",
-  integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
-})
+  integrations: [mdx(), sitemap(), solidJs(), tailwind({
+    applyBaseStyles: false
+  }), critters()]
+});
