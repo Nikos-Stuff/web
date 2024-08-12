6631496155c436b6a81fcbda55c7525eb5e6b404
_Nik0__ <maciek.f137@gmail.com>
2024-08-12 18:34:25 +0200

    ✨ Stuff

---
+++ public/js/adsense.js
@@ -0,0 +1,16 @@
+// adsense.js
+
+// Inline the AdSense script content directly
+(function() {
+    var adsbygoogle = window.adsbygoogle || [];
+    adsbygoogle.push({
+        google_ad_client: "ca-pub-4276395829672450",
+        enable_page_level_ads: true
+    });
+
+    // This is the actual AdSense script
+    var script = document.createElement('script');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+})();
