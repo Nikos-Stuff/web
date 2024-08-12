3cd1c20052cdb43634df9914852236393a93e6e3
_Nik0__ <maciek.f137@gmail.com>
2024-08-12 19:58:50 +0200

    🗑️ Testing

--- public/js/adsense.js
+++ public/js/adsense.js
@@ -1,17 +1,47 @@
 function run() {
-    var delay = 5000;
+    let delay = 5000; // 5 seconds delay
+
     setTimeout(function() {
-        var script = document.createElement('script');
+        // Create and add the AdSense script
+        let adsenseScript = document.createElement('script');
+        adsenseScript.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
+        adsenseScript.crossOrigin = "anonymous";
+        adsenseScript.async = true;
+        document.head.appendChild(adsenseScript);
 
-        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
-        script.crossOrigin = "anonymous";
-        script.async = true;
 
 
-        document.head.appendChild(script);
+        // Create and add the Funding Choices script
+        let fundingChoicesScript1 = document.createElement('script');
+        fundingChoicesScript1.src = "https://fundingchoicesmessages.google.com/i/pub-4276395829672450?ers=1";
+        fundingChoicesScript1.async = true;
+        fundingChoicesScript1.nonce = "thd-XSK7RnvUrv4fQ-RARQ";
+        document.head.appendChild(fundingChoicesScript1);
 
+        let fundingChoicesScript2 = document.createElement('script');
+        fundingChoicesScript2.text = `
+            (function() {
+                function signalGooglefcPresent() {
+                    if (!window.frames['googlefcPresent']) {
+                        if (document.body) {
+                            const iframe = document.createElement('iframe');
+                            iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                            iframe.style.display = 'none';
+                            iframe.name = 'googlefcPresent';
+                            document.body.appendChild(iframe);
+                        } else {
+                            setTimeout(signalGooglefcPresent, 0);
+                        }
+                    }
+                }
+                signalGooglefcPresent();
+            })();
+        `;
+        fundingChoicesScript2.nonce = "thd-XSK7RnvUrv4fQ-RARQ";
+        document.head.appendChild(fundingChoicesScript2);
 
         console.log('G-ADS Loaded');
+
     }, delay);
 }
 
