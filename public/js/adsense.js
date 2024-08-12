193e429467eecf706a5d28ad68ca9b7ee95a24eb
_Nik0__ <maciek.f137@gmail.com>
2024-08-12 19:39:21 +0200

    🗑️ Tweaks

--- public/js/adsense.js
+++ public/js/adsense.js
@@ -1,17 +1,19 @@
 function run() {
-    // Create a script element
-    console.log('G-ADS Loaded');
+    var delay = 3000;
+    setTimeout(function() {
+        var script = document.createElement('script');
 
-    var script = document.createElement('script');
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
+        script.crossOrigin = "anonymous";
+        script.async = true;
 
-    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
 
-    script.crossOrigin = "anonymous";
-    script.async = true;
+        document.head.appendChild(script);
 
-    document.head.appendChild(script);
+
+        console.log('G-ADS Loaded');
+    }, delay);
 }
 
-run()
-
-
+// Run the function
+run();
