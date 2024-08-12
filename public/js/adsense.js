d02b4725f3da21b410d1323bda681ff15866ce90
_Nik0__ <maciek.f137@gmail.com>
2024-08-12 19:29:45 +0200

    🔧 MINOR UPDATE - Fixing my trash lol

--- public/js/adsense.js
+++ public/js/adsense.js
@@ -1,25 +1,17 @@
 function run() {
-    // Log to indicate the function is running
+    // Create a script element
     console.log('G-ADS Loaded');
 
-    // Set the delay in milliseconds (e.g., 5000ms for 5 seconds)
-    var delay = 5000;
+    var script = document.createElement('script');
 
-    // Use setTimeout to delay the script creation and appending
-    setTimeout(function() {
-        var script = document.createElement('script');
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
 
-        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
-        script.crossOrigin = "anonymous";
-        script.async = true;
+    script.crossOrigin = "anonymous";
+    script.async = true;
 
-        // Append the script to the head
-        document.head.appendChild(script);
-
-        // Log to indicate the script is added
-        console.log('✅');
-    }, delay);
+    document.head.appendChild(script);
 }
 
-// Run the function
-run();
+run()
+
+
