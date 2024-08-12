3ca1063c605043d76577a4cd01d88e1308bf9363
_Nik0__ <maciek.f137@gmail.com>
2024-08-12 19:22:17 +0200

    🔧 MINOR UPDATE - Fixing my trash lol

--- public/js/adsense.js
+++ public/js/adsense.js
@@ -1,5 +1,7 @@
 function run() {
     // Create a script element
+    console.log('G-ADS Loaded');
+
     var script = document.createElement('script');
 
     script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4276395829672450";
