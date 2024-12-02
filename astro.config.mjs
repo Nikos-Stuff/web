1abc831404fef7080386f08fb4e36096ecdddf4b
_Nik0__ <maciek.f137@gmail.com>
2024-12-02 22:12:07 +0100

    🗑️ It should work now

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
+import critters from "astro-critters";
+//
+
 
 // https://astro.build/config
 export default defineConfig({
