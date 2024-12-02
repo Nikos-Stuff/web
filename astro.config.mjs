ea6bbdb6e6ec9e0b94367460d01690b663968f66
_Nik0__ <maciek.f137@gmail.com>
2024-12-02 22:08:14 +0100

    📂 Revert to og critters inliner

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
-import beasties from "beasties";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://nikostuff.com",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({
     applyBaseStyles: false
-  }), beasties()] // critters()
+  }), critters()]
 });
