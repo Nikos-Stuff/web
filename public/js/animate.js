7b963fa5b1cf8d220303a5d9ab2eac717f4a9095
Niko <54240096+Nik0-0@users.noreply.github.com>
2024-12-07 03:03:38 +0100

    🪄 Update animate.js

--- public/js/animate.js
+++ public/js/animate.js
@@ -10,12 +10,14 @@ function animate() {
   const animateElements = document.querySelectorAll('.animate');
 
   animateElements.forEach((element, index) => {
+   
+    const randomDelay = Math.random() * (0.1 - 0.05) + 0.05;
+
     setTimeout(() => {
       element.classList.add('show');
-    }, index * 150);
+    }, index * 150 + randomDelay * 1000);
   });
 }
 
 animate();
-
 document.addEventListener('DOMContentLoaded', animate);
