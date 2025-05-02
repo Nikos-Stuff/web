977582fa8503da70bfe025f4606af877237a73c9
_Nik0__ <maciek.f137@gmail.com>
2025-05-02 21:32:39 +0200

    ✨ Refactor animation initialization to ensure it runs after DOM content is loaded

--- public/js/animate.js
+++ public/js/animate.js
@@ -1,14 +1,13 @@
-function animate() {
+document.addEventListener('DOMContentLoaded', () => {
+  function animate() {
+    const animateElements = document.querySelectorAll('.animate');
 
-  const animateElements = document.querySelectorAll('.animate');
+    animateElements.forEach((element, index) => {
+      setTimeout(() => {
+        element.classList.add('show');
+      }, index * 450); // 150
+    });
+  }
 
-  animateElements.forEach((element, index) => {
-   
-    setTimeout(() => {
-      element.classList.add('show');
-    }, index * 450); // 150
-  });
-}
-
-animate();
-document.addEventListener('DOMContentLoaded', animate);
+  animate();
+});
