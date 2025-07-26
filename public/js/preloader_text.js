62058d4c5a50600565d26fde2398713b790ecc84
Nik0-0 <maciek.f137@gmail.com>
2025-07-26 20:05:13 +0200

    💫 Apply preloader

---
+++ public/js/preloader_text.js
@@ -0,0 +1,30 @@
+const quotes_json = "loading_quotes.json";
+
+function fetchQuotes() {
+  fetch(quotes_json)
+    .then((response) => response.json())
+    .then((data) => {
+      const quotes = data.quotes;
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      document.querySelector(
+        ".loader span"
+      ).textContent = `"${randomQuote.quote}" - ${randomQuote.author}`;
+    })
+    .catch((error) => {
+      console.error("Error fetching quotes:", error);
+      document.querySelector(".loader span").textContent =
+        "Failed to load quote.";
+    });
+}
+
+fetchQuotes();
+
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  preloader.style.opacity = "0";
+  preloader.style.pointerEvents = "none";
+
+  setTimeout(() => {
+    preloader.remove();
+  }, 1000);
+});
