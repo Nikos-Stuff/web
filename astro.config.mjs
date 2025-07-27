a9228f0e62da8d56bdfaef7f99d2e32d0bfd6e58
Nik0-0 <maciek.f137@gmail.com>
2025-07-27 21:00:48 +0200

    💫 Tweak stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   env: {
     schema: {
       NASA_API: envField.string({ context: "server", access: "secret" }),
