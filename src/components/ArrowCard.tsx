cfb9de9583ebf31d43a3780a09cdcb541fbf23a1
Nik0-0 <maciek.f137@gmail.com>
2025-10-06 13:31:17 +0200

    🔍 Some tweaks

--- src/components/ArrowCard.tsx
+++ src/components/ArrowCard.tsx
@@ -1,4 +1,4 @@
-import { formatDate } from "@lib/utils";
+import { formatDate, truncateText } from "@lib/utils";
 import type { CollectionEntry } from "astro:content";
 
 type Props = {
@@ -43,10 +43,10 @@ export default function ArrowCard({ entry, pill }: Props) {
             </div>
           )}
 
-          <div class="w-full z-10 relative group-hover:text-black group-hover:dark:text-white">
+          <div class="w-full z-10 relative text-white blend">
             <div class="flex flex-wrap items-center gap-2">
               {pill && (
-                <div class="text-sm capitalize px-2 py-0.5 rounded-full border border-black/15 dark:border-white/25">
+                <div class="text-sm capitalize px-2 py-0.5 rounded-full border border-white/25 text-white">
                   Projekty
                 </div>
               )}
@@ -65,7 +65,7 @@ export default function ArrowCard({ entry, pill }: Props) {
             <ul class="flex flex-wrap mt-2 gap-1 drop-shadow-md drop-shadow-black">
               {entry.data.tags.map((tag: string) => (
                 <li class="text-xs uppercase py-0.5 px-1 rounded bg-black/5 dark:bg-white/20 text-gray-200/75 dark:text-white/75">
-                  {tag}
+                  {truncateText(tag, 20)}
                 </li>
               ))}
             </ul>
@@ -79,7 +79,7 @@ export default function ArrowCard({ entry, pill }: Props) {
             stroke-width="2.5"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="stroke-current group-hover:stroke-black group-hover:dark:stroke-white z-10"
+            class="stroke-white z-10 drop-shadow-md drop-shadow-black"
           >
             <line
               x1="5"
@@ -100,11 +100,11 @@ export default function ArrowCard({ entry, pill }: Props) {
           href={getEntryLink()}
           class="group p-4 gap-3 flex items-center border rounded-lg hover:scale-105 hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-all duration-300 ease-in-out relative"
         >
-          <div class="w-full z-10 relative group-hover:text-black group-hover:dark:text-white">
+          <div class="w-full z-10 relative group-hover:text-black group-hover:dark:text-white transition-color duration-300 ease-in-out">
             <div class="flex flex-wrap items-center gap-2">
               {pill && (
                 <div class="text-sm capitalize px-2 py-0.5 rounded-full border border-black/15 dark:border-white/25">
-                  {entry.collection === "blog" ? "post" : "Członek Teamu"}
+                  Post
                 </div>
               )}
               <div class="text-sm uppercase">
@@ -118,7 +118,7 @@ export default function ArrowCard({ entry, pill }: Props) {
             <ul class="flex flex-wrap mt-2 gap-1">
               {entry.data.tags.map((tag: string) => (
                 <li class="text-xs uppercase py-0.5 px-1 rounded bg-black/5 dark:bg-white/20 text-black/75 dark:text-white/75">
-                  {tag}
+                  {truncateText(tag, 20)}
                 </li>
               ))}
             </ul>
@@ -150,68 +150,70 @@ export default function ArrowCard({ entry, pill }: Props) {
       ) : entry.collection === "team" ? (
         // Team Card Design
         <a
-      href={getEntryLink()}
-      class="group h-80 p-4 gap-3 flex items-center border rounded-lg hover:scale-105 hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-all duration-300 ease-in-out relative"
-    >
-      {entry.data.imageUrl && (
-        <div class="absolute rounded-lg overflow-hidden inset-0 flex items-center justify-center group-hover:scale-[0.90] transition-all duration-500 ease-in-out ">
-          <img
-            src={entry.data.imageUrl}
-            alt={entry.data.title}
-            class="parallaxImage absolute rounded-lg inset-0 w-full h-full object-cover object-center filter grayscale brightness-[0.3] blur-[4px] transition-transform duration-1000 ease-in-out will-change-transform"
-            decoding="async"
-            loading="lazy"
-          />
-        </div>
-      )}
-
-      <div class="w-full z-10 relative group-hover:text-black group-hover:dark:text-white blend">
-        <div class="flex flex-wrap items-center gap-2">
-          {pill && (
-            <div class="text-sm capitalize px-2 py-0.5 rounded-full border border-black/15 dark:border-white/25"></div>
+          href={getEntryLink()}
+          class="group h-80 p-4 gap-3 flex items-center border rounded-lg hover:scale-105 hover:bg-black/5 hover:dark:bg-white/10 border-black/15 dark:border-white/20 transition-all duration-300 ease-in-out relative"
+        >
+          {entry.data.imageUrl && (
+            <div class="absolute rounded-lg overflow-hidden inset-0 flex items-center justify-center group-hover:scale-[0.90] transition-all duration-500 ease-in-out ">
+              <img
+                src={entry.data.imageUrl}
+                alt={entry.data.title}
+                class="parallaxImage absolute rounded-lg inset-0 w-full h-full object-cover object-center filter grayscale brightness-[0.3] blur-[4px] transition-transform duration-1000 ease-in-out will-change-transform"
+                decoding="async"
+                loading="lazy"
+              />
+            </div>
           )}
-          {/* <div class="text-sm uppercase">
+
+          <div class="w-full z-10 relative text-white blend">
+            <div class="flex flex-wrap items-center gap-2">
+              {pill && (
+                <div class="text-sm capitalize px-2 py-0.5 rounded-full border border-white/25 text-white">
+                  Członek Teamu
+                </div>
+              )}
+              {/* <div class="text-sm uppercase">
                         {entry.collection === "team" ? "" : formatDate(entry.data.date)}
                     </div> */}
-        </div>
-        <div class="font-semibold mt-3 text-white drop-shadow-md drop-shadow-black">
-          {entry.data.title}
-        </div>
-        <div class="text-sm line-clamp-2 text-white drop-shadow-md drop-shadow-black">
-          {entry.data.summary}
-        </div>
-        <ul class="flex flex-wrap mt-2 gap-1 drop-shadow-md drop-shadow-black">
-          {entry.data.tags.map((tag: string) => (
-            <li class="text-xs uppercase py-0.5 px-1 rounded bg-white/20 text-white/75">
-              {tag}
-            </li>
-          ))}
-        </ul>
-      </div>
+            </div>
+            <div class="font-semibold mt-3 text-white drop-shadow-md drop-shadow-black">
+              {entry.data.title}
+            </div>
+            <div class="text-sm line-clamp-2 text-white drop-shadow-md drop-shadow-black">
+              {entry.data.summary}
+            </div>
+            <ul class="flex flex-wrap mt-2 gap-1 drop-shadow-md drop-shadow-black">
+              {entry.data.tags.map((tag: string) => (
+                <li class="text-xs uppercase py-0.5 px-1 rounded bg-white/20 text-white/75">
+                  {truncateText(tag, 20)}
+                </li>
+              ))}
+            </ul>
+          </div>
 
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="stroke-white  group-hover:stroke-white z-10"
-      >
-        <line
-          x1="5"
-          y1="12"
-          x2="19"
-          y2="12"
-          class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
-        />
-        <polyline
-          points="12 5 19 12 12 19"
-          class="translate-x-0 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
-        />
-      </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke-width="2.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="stroke-white z-10 drop-shadow-md drop-shadow-black"
+          >
+            <line
+              x1="5"
+              y1="12"
+              x2="19"
+              y2="12"
+              class="scale-x-0 group-hover:scale-x-100 translate-x-4 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
+            />
+            <polyline
+              points="12 5 19 12 12 19"
+              class="translate-x-0 group-hover:translate-x-1 transition-all duration-300 ease-in-out"
+            />
+          </svg>
         </a>
       ) : (
         // Fallback for unknown collections
