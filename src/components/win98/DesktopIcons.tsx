d5c9eb26ac878532a75f3645e74ce2e5c74bf4c3
Niko <maciek.f137@gmail.com>
2026-03-27 15:22:52 +0100

    Delete unused files

--- src/components/win98/DesktopIcons.tsx
+++
@@ -1,5 +0,0 @@
-import type { CollectionEntry } from "astro:content";
-import { createEffect, createSignal, For, onMount } from "solid-js";
-import Fuse from "fuse.js";
-import { cn } from "@lib/utils";
-
